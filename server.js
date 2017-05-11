70722ec3265b34c0fde71910c2ac857df5770feb
minakshichinda <minakshi.chinda@cognizant.com>
2017-05-11 07:46:35 +0000

    message save

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose=require('mongoose');
@@ -5,7 +6,22 @@ var app = express();
 app.use(bodyParser.json());
 mongoose.connect('mongodb://localhost:27017/mittens');
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(4000, function () {
+  console.log('Example app listening on port 4000!')
 });
 
+var Meow = mongoose.model('Meow', {
+ text: String
+  });
+
+app.get('/meows', function (req,res,next) {
+  
+  {Meow.find ({}, function(err,meows) { 
+    return.res.json(meows);
+   } );
+  });
+
+app.use(express.static('public')); 
+
+
+{}
